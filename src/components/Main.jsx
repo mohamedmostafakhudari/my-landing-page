f7c1ee835b1887a452bfdf15aae50c85d957f071
Mohamed <mohamedmostafakhudari@gmail.com>
2023-07-08 18:56:47 +0300

    start point

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Info from "./Info";
+import Quote from "./Quote";
+import CTA from "./CTA";
+export default function Main() {
+  return (
+    <main>
+      <Info />
+      <Quote />
+      <CTA />
+    </main>
+  );
+}
